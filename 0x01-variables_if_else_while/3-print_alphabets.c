f9eedd25893d5944571a598bdefbd2b0879c2a97
Fasakin Henry <fasakinhenry@gmail.com>
2023-08-11 19:10:53 -0700

    Edited solution to task 0x01c-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - This receives no argument
+ *
+ *Return: Alwaysw 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha1, alpha2;
+
+	for (al1 = 'a'; al1 <= 'z'; al1++)
+	{
+		putchar(al1);
+	}
+
+	for (al2 = 'A'; al2 <= 'Z'; 2++)
+	{
+		putchar(al2);
+	}
+	putchar('\n');
+	return (0);
+}
