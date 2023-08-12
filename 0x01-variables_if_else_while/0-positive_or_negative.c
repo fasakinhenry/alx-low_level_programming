78de6bb549259e32f1b4da2a19c3934e913f309c
Fasakin Henry <fasakinhenry@gmail.com>
2023-08-11 18:10:42 -0700

    Added betty documentation style to task 0x01c-0 task file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - receives no arguement
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
