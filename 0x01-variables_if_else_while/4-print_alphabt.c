e4a5b968954d22a76c7da5a92f9848bcab8ddcf8
Fasakin Henry <fasakinhenry@gmail.com>
2023-08-11 19:31:05 -0700

    Edited solution to task 0x01c-4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * code - main block
- * Desctiption - Use 'Putchar' to print all the letters of the alphabet except 'q' and 'e'
+ * Description: Use 'Putchar' to print all the letters except 'q' and 'e'
  * Return: 0(Success)
  */
 int main(void)
 {
 	char a = 'a';
 
-	while( a <= 'z')
+	while ( a <= 'z')
 	{
 		if ( a != 'q' && a != 'e')
 			putchar(a);
