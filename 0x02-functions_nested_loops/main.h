844b5cb8b82ef635c290c3e821ce8fdba36b416a
Fasakin Henry <fasakinhenry@gmail.com>
2023-08-15 17:48:16 -0700

    Added the main.h header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for alpha */
+void print_alphabet(void);
+
+/* Function prototype for alphax10*/
+void print_alphabet_x10(void);
+
+/* Function prototype for _isalpha*/
+int _isalpha(int c);
+
+/* Function prototype for _islower*/
+int _islower(int c);
+
+/*Function prototype for print_sign*/
+int print_sign(int n);
+
+/*Function prototype _abs*/                                           
+int _abs(int n);
+
+/*Function prototype for last_digit*/
+int print_last_digit(int n);
+
+/*Function prototype for jack_beauer*/                                           
+ void jack_bauer(void);
+
+/*Function prototype for times_table*/                                          
+void times_table(void);
+
+/*Function prototype for add*/                                          
+int add(int, int); 
+
+/*Function prototype printto98*/                                                  
+void print_to_98(int n);
+
+/*Function prototype print_times_table*/      
+void print_times_table(int n);                                              
+
+#endif /* MAIN_H */
