5f3c55683029c13c2f4720f73277f390c0d95982
Fasakin Henry <fasakinhenry@gmail.com>
2023-08-21 19:31:04 -0700

    Added the necessary files

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_
+#define MAIN_H_
+
+#include <stdlib.h>
+
+int _putchar(int);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+size_t _strlen(const char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif
