097cb2319cf4ec5657420eaf22f5ffebfff63b96
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-22 00:48:35 +0100

    [fix]: Fix indentation errors in the main.c files

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,15 @@
 section .data
     hello_msg db "Hello, Holberton", 0
+    format db "%s\n", 0  ; Adds "\n" in the format string
 
 section .text
     global main
-
-extern printf
+    extern printf
 
 main:
     push rdi                  ; Preserve the value of rdi register
     lea rdi, [hello_msg]      ; Load the address of the hello_msg string into rdi
+    lea rsi, [format]         ; Load the address of the format string into rsi
     call printf              ; Call the printf function
     pop rdi                   ; Restore the original value of rdi
     ret
