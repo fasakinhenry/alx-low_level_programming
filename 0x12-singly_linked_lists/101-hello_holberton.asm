e0d1fd4866160d593686c94347825c8a71d6f519
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-22 01:14:22 +0100

    [fix]: added new line function to hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,5 @@
 section .data
-    hello_msg db "Hello, Holberton\n", 0
-    format db "%s\n", 0  ; Adds "\n" in the format string
+    hello_msg db "Hello, Holberton", 10
 
 section .text
     global main
@@ -9,7 +8,6 @@ section .text
 main:
     push rdi                  ; Preserve the value of rdi register
     lea rdi, [hello_msg]      ; Load the address of the hello_msg string into rdi
-    lea rsi, [format]         ; Load the address of the format string into rsi
     call printf              ; Call the printf function
     pop rdi                   ; Restore the original value of rdi
     ret
