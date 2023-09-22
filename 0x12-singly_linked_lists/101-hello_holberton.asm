b361c5a82b1018c368a6a70d16b45777a30ffd6c
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-22 03:20:43 +0100

    [update] change the code of the hello holberton file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,21 @@
 extern printf
 
 section .text
-global main
+   global main
 
 main:
-    push rbp               ; Preserve the value of rbp register
+   push rbp
 
-    mov rdi, fmt           ; Load the format string address into rdi
-    mov rsi, msg           ; Load the message address into rsi
-    mov rax, 0             ; Set rax to 0 (for printf return value of characters printed)
-    call printf            ; Call the printf function
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
 
-    pop rbp                ; Restore the original value of rbp
+   pop rbp
 
-    mov rax, 60            ; syscall: exit
-    xor rdi, rdi           ; status: 0
-    syscall
+   mov rax,0
+   ret
 
 section .data
-msg: db "Hello, Holberton", 0
-fmt: db "%s", 10, 0       ; The format string with "%s" for string and '\n' (10) for newline
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
