330781c8f3651b001be4cfd30a91d7ac6e2a5cc3
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-25 21:04:16 +0100

    Feature: Add the task 3 main.c file and 3-factorial file
    functions that gives the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial -  returns factorial of a given num using recursion
+ * @n: number whose factorial is needed
+ *
+ * Return: factorial of a given number
+ * when n < 0 returns -1
+ * when n = 0 returns 1
+*/
+
+int factorial(int n)
+{
+	/* Base case */
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return(n * factorial(n - 1));
+}
