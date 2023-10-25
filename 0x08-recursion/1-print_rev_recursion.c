3d401aff7e8a8305fb13b29bb170463b58bc518c
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-25 19:54:13 +0100

    Feature: finish task 1
    function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse,
+ * followed by a new line using recursion
+ * @s: string parameter to be printed
+*/
+
+void _print_rev_recursion(char *s)
+{
+	/* Set the base case */
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
