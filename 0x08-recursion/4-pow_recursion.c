f19ddbcc7dd849d22492097fc92f55819ef34fbe
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-25 21:07:36 +0100

    Feature: Add the task 4 main,c file and pow_recursion c file
    functions that returns power of a number using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to power y
+ * @x: value of x
+ * @y: power to which x will be raised to
+ * 
+ * Return: value of x raised to the power of y
+ * if y < 0, return -1
+*/
+
+int _pow_recursion(int x, int y)
+{
+	/* Base case*/
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, (y-1)));
+}
