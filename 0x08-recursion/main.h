017c4b20a844b2633df7dca24492826720dc9816
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-25 20:22:00 +0100

    Feature: Add the function prototype for the strlen_ recursion function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,8 @@ void _puts_recursion(char *s);
 /* Function prototype for put reverse recursion */
 void _print_rev_recursion(char *s);
 
-/**/
+/*Function prototype for str lenght recursion */
 int _strlen_recursion(char *s);
+
+/* */
 #endif
