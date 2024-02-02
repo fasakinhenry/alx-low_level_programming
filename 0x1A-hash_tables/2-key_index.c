2eb7cbd98da590b7e5e3aad0dda1310a120a0f8b
Fasakin Henry <fasakinhenry@gmail.com>
2024-02-02 03:13:42 +0100

    add the new required directory

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Gets the index at which a value
+ * key_index - Get the index at which a key/value
  *      pair should be stored in array of a hash table.
- * @key: Key to get the index of.
+ * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
  * Return: The index of the key.
@@ -11,5 +11,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,36 +4,36 @@
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
  *
- * Description: Value pairs are printed in the order
+ * Description: Key/value pairs are printed in the order
  *              they appear in the array of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *node;
-    unsigned long int i;
-    unsigned char comma_flag = 0;
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            if (comma_flag == 1)
-                printf(", ");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
 
-            node = ht->array[i];
-            while (node != NULL)
-            {
-                printf("'%s': '%s'", node->key, node->value);
-                node = node->next;
-                if (node != NULL)
-                    printf(", ");
-            }
-            comma_flag = 1;
-        }
-    }
-    printf("}\n");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
 }
